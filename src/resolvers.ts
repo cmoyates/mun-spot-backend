29da0c20894e7617762db2cbf8caf65bd260f1e9
Cristopher Yates <cmoyates@gmail.com>
2022-06-24 10:01:26 -0230

    Added ability to get all courses in a subject

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,52 +1,62 @@
 import BannerOffering, { IBannerOffering } from "./models/BannerOffering";
-import "./db"
+import "./db";
 import CalendarCourse, { ICalendarCourse } from "./models/CalendarCourse";
 import { getRMPRating, courseSearch, courseAutocomplete } from "./helpers";
 
-type GetOfferingsQuery = { subject_code: string, number: string, campus?: string }
+type GetOfferingsQuery = {
+  subject_code: string;
+  number: string;
+  campus?: string;
+};
 
 export const resolvers = {
-    Query: {
-        getOfferings: async (_parent: object, { subject_code, number, campus }: GetOfferingsQuery) => {
-            const query: GetOfferingsQuery = { subject_code, number };
-            if (campus) query.campus = campus;
-            const offerings: IBannerOffering[] = await BannerOffering
-                .find(query)
-                .sort({
-                    year: -1,
-                    term: -1
-                })
-                .exec();
-            for await (const offering of offerings) {
-                if (offering.rmp !== undefined || !offering.prof_full) continue;
-                offering.rmp = await getRMPRating(offering.prof_full);
-                await offering.save();
-            }
-            let offeringsMap: Map<string, IBannerOffering[]> = new Map<string, IBannerOffering[]>();
-            offerings.forEach((offering)=>{
-                const offeringGroupString = `${offering.year}-${offering.term}-${offering.campus}`;
-                if (offeringsMap.get(offeringGroupString)) {
-                    offeringsMap.get(offeringGroupString)?.push(offering)
-                }
-                else {
-                    offeringsMap.set(offeringGroupString, [offering])
-                }
-            });
-            return offeringsMap.values();
-        },
-        getCourseDetails: async (_parent: object, { subject, number }: { subject: string, number: string }) => {
-            const query = { subject, number };
-            const courses: ICalendarCourse[] = await CalendarCourse
-                .find(query)
-                .limit(5)
-                .exec();
-            return courses;
-        },
-        courseSearch: async (_parent: object, { query, subject }: { query: string, subject: string }) => {
-            return query ? await courseSearch(query, subject) : [];
-        },
-        courseAutocomplete: async (_parent: object, { subject, number }: { subject: string, number: string }) => {
-            return number ? await courseAutocomplete(subject, number) : [];
+  Query: {
+    getOfferings: async (_parent: object, query: GetOfferingsQuery) => {
+      const offerings: IBannerOffering[] = await BannerOffering.find(query)
+        .sort({
+          year: -1,
+          term: -1,
+        })
+        .exec();
+      for await (const offering of offerings) {
+        if (offering.rmp !== undefined || !offering.prof_full) continue;
+        offering.rmp = await getRMPRating(offering.prof_full);
+        await offering.save();
+      }
+      const offeringsMap: Map<string, IBannerOffering[]> = new Map<
+        string,
+        IBannerOffering[]
+      >();
+      offerings.forEach((offering) => {
+        const offeringGroupString = `${offering.year}-${offering.term}-${offering.campus}`;
+        if (offeringsMap.get(offeringGroupString)) {
+          offeringsMap.get(offeringGroupString)?.push(offering);
+        } else {
+          offeringsMap.set(offeringGroupString, [offering]);
         }
-    }
+      });
+      return offeringsMap.values();
+    },
+    getCourseDetails: async (
+      _parent: object,
+      query: { subject: string; number?: string }
+    ) => {
+      const courses: ICalendarCourse[] = await CalendarCourse.find(query)
+        .limit(5)
+        .exec();
+      return courses;
+    },
+    courseSearch: async (
+      _parent: object,
+      { query, subject }: { query: string; subject: string }
+    ) => {
+      return query ? await courseSearch(query, subject) : [];
+    },
+    courseAutocomplete: async (
+      _parent: object,
+      { subject, number }: { subject: string; number: string }
+    ) => {
+      return number ? await courseAutocomplete(subject, number) : [];
+    },
+  },
 };
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,77 +1,81 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-    type Times {
-        sunday: [String],
-        monday: [String],
-        tuesday: [String],
-        wednesday: [String],
-        thursday: [String],
-        friday: [String],
-        saturday: [String]
-    }
+  type Times {
+    sunday: [String]
+    monday: [String]
+    tuesday: [String]
+    wednesday: [String]
+    thursday: [String]
+    friday: [String]
+    saturday: [String]
+  }
 
-    type RMPRating {
-        _id: ID,
-        query: String,
-        rating: String,
-        rating_count: String,
-        legacy_id: String
-    }
+  type RMPRating {
+    _id: ID
+    query: String
+    rating: String
+    rating_count: String
+    legacy_id: String
+  }
 
-    type ReservedFor {
-        degree: [String],
-        major: [String],
-        minor: [String]
-    }
+  type ReservedFor {
+    degree: [String]
+    major: [String]
+    minor: [String]
+  }
 
-    type BannerOffering {
-        _id: ID,
-        prof: String,
-        prof_full: String,
-        crn: String,
-        room: String,
-        type: String,
-        times: Times,
-        notes: [String],
-        campus: String,
-        subject: String,
-        subject_code: String,
-        number: String,
-        section: String,
-        rmp: RMPRating,
-        associated_sections: [String],
-        cross_listed: [String],
-        reserved_for: ReservedFor,
-        year: Int,
-        term: Int
-    }
+  type BannerOffering {
+    _id: ID
+    prof: String
+    prof_full: String
+    crn: String
+    room: String
+    type: String
+    times: Times
+    notes: [String]
+    campus: String
+    subject: String
+    subject_code: String
+    number: String
+    section: String
+    rmp: RMPRating
+    associated_sections: [String]
+    cross_listed: [String]
+    reserved_for: ReservedFor
+    year: Int
+    term: Int
+  }
 
-    type CourseAttributes {
-        AR: String,
-        CH: String,
-        CO: String,
-        CR: String,
-        LC: String,
-        LH: String,
-        OR: String,
-        PR: String,
-        UL: String
-    }
+  type CourseAttributes {
+    AR: String
+    CH: String
+    CO: String
+    CR: String
+    LC: String
+    LH: String
+    OR: String
+    PR: String
+    UL: String
+  }
 
-    type CalendarCourse {
-        _id: ID,
-        name: String,
-        number: String,
-        description: String,
-        subject: String,
-        attributes: CourseAttributes
-    }
+  type CalendarCourse {
+    _id: ID
+    name: String
+    number: String
+    description: String
+    subject: String
+    attributes: CourseAttributes
+  }
 
-    type Query {
-        getOfferings(subject_code: String!, number: String!, campus: String): [[BannerOffering]],
-        getCourseDetails(subject: String!, number: String!): [CalendarCourse],
-        courseSearch(query: String!, subject: String): [CalendarCourse],
-        courseAutocomplete(subject: String!, number: String): [CalendarCourse]
-    }
+  type Query {
+    getOfferings(
+      subject_code: String!
+      number: String!
+      campus: String
+    ): [[BannerOffering]]
+    getCourseDetails(subject: String!, number: String): [CalendarCourse]
+    courseSearch(query: String!, subject: String): [CalendarCourse]
+    courseAutocomplete(subject: String!, number: String): [CalendarCourse]
+  }
 `;
